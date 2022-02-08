b4e6e49de19f8911505a16995fdba444cae7886a
Mikhail Nosov <manosov1984@gmail.com>
2022-02-09 01:23:21 +0400

    feat: add ;

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@
 
 module.exports = function towelSort (matrix) {
   let arr = [];
-  let result = []
-  if (matrix === null || matrix === undefined) result = arr
+  let result = [];
+  if (matrix === null || matrix === undefined) result;
   if (matrix) {
     for (let i = 0; i < matrix.length; i++) {
       if ( i % 2 === 0) {
         for (let j = 0; j < matrix[i].length; j++) {
-          arr.push(matrix[i][j])
+          arr.push(matrix[i][j]);
         }
       } else {
-        for (let j = matrix[i].length-1; j >= 0; j--) {
-          arr.push(matrix[i][j])
+        for (let j = matrix[i].length - 1; j >= 0; j--) {
+          arr.push(matrix[i][j]);
         }
       }
-    result = arr
+    result = arr;
     }
   }
-  
-  return result
+  return result;
 }
